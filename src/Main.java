a8cbd1f43e6425366a87b37bd14c02d9eea7fefb
alexjununo <alex.jpp10@gmail.com>
2022-08-01 12:23:21 -0300

    nomes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 import Handlers.Menus;
 
+/**
+ * @author Alex Pereira e Gloria Grecco
+ * @version 1.0
+ *  
+ * Classe principal do programa
+ * 
+ * ADS - Trabalho de Linguagem de Programação Orientada a Objetos I
+ */
+
 public class Main {
     public static void main(String[] args) {
         Menus menu = new Menus();
